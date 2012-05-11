f17c25f36bdeea3ef9653ce4621186423d4c8213
Andrea Catalucci <a.catalucci@gmail.com>
2012-05-11 16:57:45 +0200

    comments added

--- step1.rb
+++ step1.rb
@@ -4,15 +4,23 @@ dataset_folder_fullpath = "/mnt/opt/data/pp1_12_exercise/dataset/fasta"
 prot_list_file_fullpath = "/mnt/opt/data/pp1_12_exercise/groups/101/101.txt"
 FileUtils.rm_rf 'ppdir'
 Dir.mkdir 'ppdir'
+
+#this array contains all the protein names as specified in the file
 proteins = []
+
+#i read all the sequences and load them in memory: it's simple, efficient and requires
+#not much memory
 File.open(prot_list_file_fullpath, 'r') do |infile|
   while (prot_seq_name = infile.gets)
     proteins = proteins << prot_seq_name.chomp
   end
 end
 
+#this array memorizes the threads in process
 threads = []
+
 proteins.each do |prot_seq_name|
+  #i create a new thread for every sequence
   threads << Thread.new(prot_seq_name) do |prot_seq_name|
     protein_file_fullpath = "#{dataset_folder_fullpath}/#{prot_seq_name}"
     uniprot_code = "UNIDENTIFIED"
@@ -21,6 +29,7 @@ proteins.each do |prot_seq_name|
     command = "/usr/bin/predictprotein --seqfile #{protein_file_fullpath} --target=all --target=optional --output-dir #{out_dir_path}  --nouse-cache --bigblastdb=/var/tmp/rost_db/data/big/big --big80blastdb=/var/tmp/rost_db/data/big/big_80 --pfam2db=/var/tmp/rost_db/data/pfam_legacy/Pfam_ls --pfam3db=/var/tmp/rost_db/data/pfam/Pfam-A.hmm --prositeconvdat=/var/tmp/rost_db/data/prosite/prosite_convert.dat --prositedat=/var/tmp/rost_db/data/prosite/prosite.dat --swissblastdb=/var/tmp/rost_db/data/swissprot/uniprot_sprot"
     system(command)
   end
+    #every 4 threads i stop creating new threads and wait for the older ones to complete
     if threads.size == 4
       threads.each {|t| t.join} 
       threads = []
