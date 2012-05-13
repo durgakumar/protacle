9464ce222c8d6d3cd8bf2903aa4cad0ad31bbf4a
Andrea Catalucci <a.catalucci@gmail.com>
2012-05-13 17:51:11 +0200

    added an utility script to dispatch jobs among all the machines

---
+++ dispatch.rb
@@ -0,0 +1,24 @@
+#this little script takes 2 parameters: 1 - the name of the input file and 2 -the command that it has to run
+#the script connects to all the servers in the labs and calls the provided command on each server for a subset of
+#the provided input. requires ssh and sshpass (linux system, probably works on mac too)
+
+USERNAME="cataluccia"
+PASSWORD="nrStYrntzJ1I"
+KILLCOMMAND="killall -u #{USERNAME}"
+
+input_size = ARGV[1].to_i
+remote_command = ARGV[0]
+
+machine_numbers = ["01","02","03","06","07","08"]
+
+step_size = (input_size / machine_numbers.size).floor
+start_index = 0
+end_index = step_size
+
+machine_numbers.each do |machine_num|
+  end_index = input_size-1 if machine_num == machine_numbers.last
+  command = "sshpass -p #{PASSWORD} ssh \"#{USERNAME}@i12k-biolab#{machine_num}.informatik.tu-muenchen.de\" \"nohup ruby #{remote_command} #{start_index} #{end_index} > machine#{machine_num}.out &\""
+  system(command)
+  end_index += step_size
+  start_index = end_index - step_size + 1
+end
